a1250b273fb9be1392dd3b58df6d567f0dbbab6f
Andy Ibanez <codecs.elision09@icloud.com>
2022-03-07 09:43:41 -0400

    Initial Commit

---
+++ nookapi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nookapi
+//
+//  Created by Andy Ibanez on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nookapi/nookapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nookapiApp.swift
+//  nookapi
+//
+//  Created by Andy Ibanez on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct nookapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
